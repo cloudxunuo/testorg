e95b0fc5418a8cdc1f3cbadb1a7c79ef2938746e
cloudxunuo@gmail.com <cloudxunuo@gmail.com@737d364e-a85a-6ec3-750b-a2275dba1f86>
2013-01-16 09:47:20 +0000

    解决conflict

--- WebRoot/js/coolGrid.js
+++ WebRoot/js/coolGrid.js
@@ -1,5 +1,6 @@
 //Our coolGrid jquery plugins
 //Author:Xuezi Zhang and Qiang Xu
+
 (function($) { 
 	// 
 	// plugin definition 
@@ -15,18 +16,70 @@
 		var pageParams = "{currentPage:1,pageSize:5,totalPage:1}";
 		var queryParams = "[{name:'MAIN_ID',value:'SQ20120903ADMI0002'}]";
 		var sortParams = "{sortCol:'MAIN_ID',order:'asc'}";
-		loadTableData(pageParams,queryParams,sortParams);
+		//loadTableData(pageParams,queryParams,sortParams);
 		//var data = {"dataSet":[{"ID":"CG001","STATUS":"N","AUDITOR":"李峰","REAL_AUDITOR":"许强","VERIFY_DESC":"审核不合格","VERIFY_DATE":"2013-01-04","VERIFY_DEPART":"一车间","VERIFY_LEVEL":"部门经理","FIELD1":"1","FIELD2":"1"},{"ID":"CG002","STATUS":"N","AUDITOR":"李峰","REAL_AUDITOR":"许强","VERIFY_DESC":"审核不合格","VERIFY_DATE":"2013-01-04","VERIFY_DEPART":"一车间","VERIFY_LEVEL":"部门经理","FIELD1":"1","FIELD2":"1"},{"ID":"CG003","STATUS":"N","AUDITOR":"李峰","REAL_AUDITOR":"许强","VERIFY_DESC":"审核不合格","VERIFY_DATE":"2013-01-04","VERIFY_DEPART":"一车间","VERIFY_LEVEL":"部门经理","FIELD1":"1","FIELD2":"1"},{"ID":"CG004","STATUS":"N","AUDITOR":"李峰","REAL_AUDITOR":"许强","VERIFY_DESC":"审核不合格","VERIFY_DATE":"2013-01-04","VERIFY_DEPART":"一车间","VERIFY_LEVEL":"部门经理","FIELD1":"1","FIELD2":"1"},{"ID":"CG005","STATUS":"N","AUDITOR":"李峰","REAL_AUDITOR":"许强","VERIFY_DESC":"审核不合格","VERIFY_DATE":"2013-01-04","VERIFY_DEPART":"一车间","VERIFY_LEVEL":"部门经理","FIELD1":"1","FIELD2":"1"},{"ID":"CG006","STATUS":"Y","AUDITOR":"马建山","REAL_AUDITOR":"许强","VERIFY_DESC":"审核合格","VERIFY_DATE":"2013-01-04","VERIFY_DEPART":"一车间","VERIFY_LEVEL":"部门经理","FIELD1":"1","FIELD2":"1"},{"ID":"CG007","STATUS":"Y","AUDITOR":"马建山","REAL_AUDITOR":"许强","VERIFY_DESC":"审核合格","VERIFY_DATE":"2013-01-04","VERIFY_DEPART":"一车间","VERIFY_LEVEL":"部门经理","FIELD1":"1","FIELD2":"1"},{"ID":"CG008","STATUS":"Y","AUDITOR":"马建山","REAL_AUDITOR":"许强","VERIFY_DESC":"审核合格","VERIFY_DATE":"2013-01-04","VERIFY_DEPART":"一车间","VERIFY_LEVEL":"部门经理","FIELD1":"1","FIELD2":"1"},{"ID":"CG009","STATUS":"Y","AUDITOR":"马建山","REAL_AUDITOR":"许强","VERIFY_DESC":"审核合格","VERIFY_DATE":"2013-01-04","VERIFY_DEPART":"一车间","VERIFY_LEVEL":"部门经理","FIELD1":"1","FIELD2":"1"}],pageParams:{"currentPage":"1","totalPage":"2"}};
 		
 		//addData2Table(data, this);
+		
+		bindEvents(this);
 	};
 	
+	function bindEvents($obj){
+		$obj.find(".add").bind("click",onAddClick);
+		$obj.find(".delete").bind("click",onDeleteClick);
+		$obj.find(".update").bind("click",onUpdateClick);
+	}
+	
+	function onAddClick(event){
+		var $firstTR = $(event.target).parents("tr").filter(":first");
+		var colModel = $.fn.coolGrid.options.colModel;
+		var subTableCount = colModel.length;
+		if (subTableCount == 1){
+			//如果是简单表
+			var data = $firstTR.find(":input").serializeArray();
+		}
+		else{
+			//如果是复杂表
+			var firstData = $firstTR.find("::input:hidden").serializeArray();
+			var param = {opParam:"delete"};
+			param.dataTable = $.fn.coolGrid.options.databaseTableName;
+		}
+	}
+	function onDeleteClick(){
+		var $firstTR = $(event.target).parents("tr").filter(":first");
+		var clickRowIndex =  $firstTR[0].rowIndex;
+		var colModel = $.fn.coolGrid.options.colModel;
+		var subTableCount = colModel.length;
+		var data = [{"":""}];
+		if (subTableCount == 1){
+			//如果是简单表
+			data = $firstTR.find(":input:hidden").serializeArray();
+		}
+		else{
+			//如果是复杂表
+			for (var j = 0; j < subTableCount; j++){
+	    		var $tmpTR = $("#subTable" + j + " tr :eq(" + clickRowIndex + ")");
+	    		var tmpData = $tmpTR.find(":input:hidden").serializeArray();
+	    		for (var key in tmpData){
+    				data.push({name:tmpData[key]["name"],value:tmpData[key]["value"]});
+	    		}
+			}
+		}
+		var param = {opParam:"delete"};
+		param.dataTable = $.fn.coolGrid.options.databaseTableName;
+		param.queryParams = data;
+		alert(param);
+		console.log(param);
+	}
+	function onUpdateClick(){
+		
+	}
 	
 	
 	function debug($obj) { 
 		if (window.console && window.console.log) 
 		window.console.log('coolGrid selection count: ' + $obj.size()); 
-	};
+	}
 
 	function drawTableHeader($obj){
 		//最简单的table
@@ -120,16 +173,21 @@
 			    			$tr = $obj.find("tr :last");
 			    			$tr.append("<td></td>");
 			    			$td = $tr.find("td :last");
-			    			if (table.data[k].key != undefined)
-			    				$td.attr("key",table.data[k].name);
-			    			var types = table.data[k].type.split("|");
 			    			var colName = table.data[k].name;
+			    			if (table.data[k].key != undefined)
+			    				$td.append("<input type='hidden' name='"+colName+"' value='"+data.dataSet[m][colName]+"'></input>");
+			    			var types = table.data[k].type.split("|");
 			    			for (var n = 0; n < types.length; n++){
 			    				switch(types[n]){
-			    				case "data": ;$td.append(data.dataSet[m][colName]);break;
-			    				case "insert": $td.append("<a href='javascript:insert(this)'><img src='images/add.gif' border='0' alt='添加记录'></a>");break;
-			    				case "delete": $td.append("<a href='javascript:delete(this)'><img src='images/delete.gif' border='0' alt='删除记录'></a>");break;
-			    				case "update": $td.append("<a href='javascript:update(this)'><img src='images/update.gif' border='0' alt='删除记录'></a>");break;
+			    				case "data":
+			    					if(table.data[k].editable == undefined)
+			    						$td.append("<input style='width:90%;' type='text' name='" + colName + "' value='"+ data.dataSet[m][colName] +"'>");
+			    					else
+			    						$td.append(data.dataSet[m][colName]);
+			    					break;
+			    				case "insert": $td.append("<a href='#' class='insert'><img src='images/add.gif' border='0' alt='添加记录'></a>");break;
+			    				case "delete": $td.append("<a href='#' class='delete'><img src='images/delete.gif' border='0' alt='删除记录'></a>");break;
+			    				case "update": $td.append("<a href='#' class='update'><img src='images/update.gif' border='0' alt='删除记录'></a>");break;
 			    				}
 			    			}
 			    		}
@@ -154,16 +212,21 @@
 				    		for (var k = 0; k < subTable.data.length; k++){
 				    			$("#subTable" + j + " tr :last").append("<td></td>");
 				    			$td = $("#subTable" + j + " tr :last td :last");
-				    			if (subTable.data[k].key != undefined)
-				    				$td.attr("key",subTable.data[k].name);
-				    			var types = subTable.data[k].type.split("|");
 				    			var colName = subTable.data[k].name;
+				    			if (subTable.data[k].key != undefined)
+				    				$td.append("<input type='hidden' name='"+colName+"' value='"+data.dataSet[m][colName]+"'></input>");
+				    			var types = subTable.data[k].type.split("|");
 				    			for (var n = 0; n < types.length; n++){
 				    				switch(types[n]){
-				    				case "data": ;$td.append(data.dataSet[m][colName]);break;
-				    				case "insert": $td.append("<a href='javascript:insert(this)'><img src='images/add.gif' border='0' alt='添加记录'></a>");break;
-				    				case "delete": $td.append("<a href='javascript:delete(this)'><img src='images/delete.gif' border='0' alt='删除记录'></a>");break;
-				    				case "update": $td.append("<a href='javascript:update(this)'><img src='images/update.gif' border='0' alt='删除记录'></a>");break;
+				    				case "data":
+				    					if(subTable.data[k].editable == undefined)
+				    						$td.append("<input style='width:90%;' type='text' name='" + colName + "' value='"+ data.dataSet[m][colName] +"'>");
+				    					else
+				    						$td.append(data.dataSet[m][colName]);
+				    					break;
+				    				case "insert": $td.append("<a href='#' class='insert'><img src='images/add.gif' border='0' alt='添加记录'></a>");break;
+				    				case "delete": $td.append("<a href='#' class='delete'><img src='images/delete.gif' border='0' alt='删除记录'></a>");break;
+				    				case "update": $td.append("<a href='#' class='update'><img src='images/update.gif' border='0' alt='删除记录'></a>");break;
 				    				}
 				    			}
 				    		}
@@ -175,12 +238,53 @@
 		}
 		
 		if ($.fn.coolGrid.options.insertable != undefined){
+			//是否只是可添加数据
 			if (subTableCount == 1){
 				//如果是简单表
-				$obj.append("<tr></tr>");
+				var rowCount = $obj.children("tr").length;
+				if (rowCount % 2 != 0)
+					$obj.append("<tr class='tabtd1'></tr>");
+				else
+					$obj.append("<tr class='tabtd2'></tr>");
+				$tr = $obj.find("tr :last");
+				for (var k = 0; k < colModel[0].data.length; k++){
+					$tr.append("<td></td>");
+					$td = $tr.find("td :last");
+					if (k == 0)
+						$td.append("<a href='#' class='add'><img src='images/add.gif' border='0' alt='添加记录'></a>");
+					else{
+						$td.append("<input style='width:90%;' type='text' name='" + colModel[0].data[k].name + "'>");
+					}
+				}
 			}
 			else{
 				//如果是复杂表
+				var rowCount = $("subTable0 tr").length;
+				if (rowCount % 2 != 0)
+		    	{
+		    		for (var j = 0; j < subTableCount; j++){
+		    			$("#subTable" + j).append("<tr class='tabtd1'></tr>");
+		    		}
+		    	}
+		    	else
+		    	{
+		    		for (var j = 0; j < subTableCount; j++){
+		    			$("#subTable" + j).append("<tr class='tabtd2'></tr>");
+		    		}
+		    	}
+				for (var j = 0; j < subTableCount; j++){
+		    		var subTable = colModel[j];
+		    		for (var k = 0; k < subTable.data.length; k++){
+		    			$("#subTable" + j + " tr :last").append("<td></td>");
+		    			$td = $("#subTable" + j + " tr :last td :last");
+		    			var colName = subTable.data[k].name;
+		    			if (k == 0 && j ==0)
+							$td.append("<a href='#' class='add'><img src='images/add.gif' border='0' alt='添加记录'></a>");
+						else{
+							$td.append("<input style='width:90%;' type='text' name='" + colName + "'>");
+						}
+		    		}
+		    	}
 			}
 		}
 	}
@@ -221,33 +325,6 @@
 			}
 		);
 				
-		
-	}
-
-	
-	function thisDelete(queryParams){
-		var url = options.url;		
-		var dataTable = options.databaseTableName;		
-		var params = {opParam:"delete",dataTable:dataTable,queryParams:queryParams};
-		
-		$.ajax({
-			url:url,//发送请求地址
-			data:$.toJSON(params),
-			type:'post',//请求方式
-			dataType:'json',
-			contentType: 'application/json;charset=utf-8',
-			success:function(){
-				loadTableData();
-			},
-			error: function(xhr) {alert(url + " handler got fucked up!");}
-		});
-	}
-	
-	function insertNew(){
-		
-	}
-	
-	function editThis(){
 		
 	}
 	$.fn.coolGrid.options = {};
